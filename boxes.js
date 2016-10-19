70a6ee1c987c80c12a1224b690a17d61045c4520
Evan Busse <evanbusse@gmail.com>
2016-10-19 15:37:20 -0600

    Exposed the ecret box

--- boxes.js
+++ boxes.js
@@ -1,1 +1,13 @@
 //write your code here
+'use strict';
+
+$(document).ready(function() {
+
+  $('#secretBox').css("background-color", "white");
+  $('#secretBox').append("<h1>Secret Box!</h1>");
+
+});
+
+$('.boxType1').css('background', 'white');
+
+$('#row4').rem
