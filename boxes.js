567821fb2fdb8e16ce4f10c488044508b70cba40
Evan Busse <evanbusse@gmail.com>
2016-10-20 10:30:25 -0600

    Toggles like no other

--- boxes.js
+++ boxes.js
@@ -8,6 +8,28 @@ $(document).ready(function() {
 
 });
 
-$('.boxType1').css('background', 'white');
+//# 1
+// $('.boxType1').css('background', 'white');
 
-$('#row4').rem
+//# 2
+// $('#row4').css('display', 'false');
+
+//# 3
+// var children = $('#row1').children();
+// console.log(children.length);
+
+//# 4
+// $('#row4 div:last').css('display', 'none');
+
+//# 5
+// var firstTwo = $('#row1 div').slice(0,2);
+// $(firstTwo).toggleClass('box');
+
+//# 6
+// $('#container').children().children().not('#secretBox').css('width', '2px');
+// console.log('true');
+
+//# 7
+$('body').toggle("slow", function(){
+  $(this).toggle("slow");
+})
